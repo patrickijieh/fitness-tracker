49d7b8e5175504fb5ef3fab93d0ade00a05b6c9f
Patrick Ijieh <ijiehpatrick@gmail.com>
2025-07-06 16:41:13 -0400

    Initial Commit

---
+++ fitness-tracker/fitness_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_trackerApp.swift
+//  fitness-tracker
+//
+//  Created by Patrick Ijieh on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct fitness_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
