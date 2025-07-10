c22337891742ea08f3448e621c64eef308fc95f0
Patrick Ijieh <ijiehpatrick@gmail.com>
2025-07-10 05:31:12 -0400

    Add basic welcome screen

---
+++ fitness-tracker/WelcomeScreen.swift
@@ -0,0 +1,33 @@
+//
+//  Home.swift
+//  fitness-tracker
+//
+//  Created by Patrick Ijieh on 7/7/25.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint);
+            Text("Welcome to the Fitness Macro Tracker app!")
+                .padding(30)
+                .multilineTextAlignment(.center);
+            Button(action: gotoHome) {
+                Text("Get Started")
+            }
+        }
+        .padding()
+    }
+}
+
+func gotoHome() {
+    return;
+}
+
+#Preview {
+    Welcome()
+}
